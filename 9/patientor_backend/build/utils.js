e26cb4f8399c37f774c845ff9f7302c1ec247382
kukko100 <karim_el-zaben@hotmail.com>
2023-10-28 11:23:18 +0300

    Revert "Revert "asdf""
    This reverts commit 3be6b6540aedf62d02b803ef37ef81a0d8771455.

--- 9/patientor_backend/build/utils.js
+++ 9/patientor_backend/build/utils.js
@@ -1,21 +1,55 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const types_1 = require("./types");
-const toNewPatientEntry = (object) => {
+const toNewPatient = (object) => {
     if (!object || typeof object !== 'object') {
         throw new Error('Incorrect or missing data');
     }
-    if ('name' in object && 'dateOfBirth' in object && 'gender' in object && 'occupation' in object) {
-        const newEntry = {
+    if ('name' in object && 'dateOfBirth' in object && 'gender' in object && 'occupation' in object && 'ssn' in object && 'entries' in object) {
+        const newPatient = {
             name: parseName(object.name),
-            dateOfBirth: parseDateOfBirth(object.dateOfBirth),
+            dateOfBirth: parseDate(object.dateOfBirth),
             gender: parseGender(object.gender),
-            occupation: parseOccupation(object.occupation)
+            occupation: parseOccupation(object.occupation),
+            ssn: parseSSN(object.ssn),
+            entries: parseEntry(object.entries),
+        };
+        return newPatient;
+    }
+    throw new Error('Incorrect data: some fields are missing in new Patient');
+};
+const toNewEntry = (object) => {
+    if (!object || typeof object !== 'object') {
+        throw new Error('incorrect or missing data');
+    }
+    if ('description' in object && 'date' in object && 'specialist' in object && 'diagnosisCodes' in object && 'discharge' in object) {
+        const newEntry = {
+            description: parseDescription(object.description),
+            date: parseDate(object.date),
+            specialist: parseSpecialist(object.specialist),
+            diagnosisCodes: parseDiagnosisCodes(object.diagnosisCodes),
+            discharge: parseDischarge(object.discharge),
         };
         return newEntry;
     }
-    throw new Error('Incorrect data: some fields are missing');
+    throw new Error('Incorrect data: some fields are missing in new Entry');
 };
+// const toNewHealthCheckEntry = (object: unknown): NewHealthCheckEntry => {
+//   if ( !object || typeof object !== 'object' ) {
+//     throw new Error('incorrect or missing data');
+//   }
+//   if ('description' in object && 'date' in object && 'specialist' in object && 'healthCheckRating' && 'type' in object) {
+//     const newEntry: NewHealthCheckEntry = {
+//       description: parseDescription(object.description),
+//       date: parseDate(object.date), 
+//       specialist: parseSpecialist(object.specialist),
+//       healthCheckRating: parseHealthCheckRating(object.healthCheckRating),
+//       type: parseType(object.type)
+//     };
+//     return newEntry;
+//   }
+//   throw new Error('Incorrect data: some fields are missing in new Entry');
+// };
 const isString = (text) => {
     return typeof text === 'string' || text instanceof String;
 };
@@ -25,13 +59,34 @@ const isDate = (date) => {
 const isGender = (gender) => {
     return Object.values(types_1.Gender).map(v => v.toString()).includes(gender);
 };
+const isEntry = (entries) => {
+    if (entries && typeof entries === 'object' && 'type' in entries) {
+        return true;
+    }
+    return false;
+};
+const isDischarge = (discharge) => {
+    if (discharge && typeof discharge === 'object' && 'criteria' in discharge) {
+        return true;
+    }
+    return false;
+};
+const isStringArray = (array) => {
+    return Array.isArray(array) && array.every(item => typeof item === 'string');
+};
+// const isHealthCheckRating = (healthCheckRating: unknown): healthCheckRating is HealthCheckRating => {
+//   if (healthCheckRating && typeof healthCheckRating === 'object') {
+//     return true;
+//   }
+//   return false;
+// };
 const parseName = (name) => {
     if (!name || !isString(name)) {
         throw new Error('Incorrect or missing name');
     }
     return name;
 };
-const parseDateOfBirth = (date) => {
+const parseDate = (date) => {
     if (!date || !isString(date) || !isDate(date)) {
         throw new Error('Incorrect or missing date: ' + date);
     }
@@ -49,4 +104,46 @@ const parseOccupation = (occupation) => {
     }
     return occupation;
 };
-exports.default = toNewPatientEntry;
+const parseSSN = (ssn) => {
+    if (!ssn || !isString(ssn)) {
+        throw new Error('Incorrect or missing ssn');
+    }
+    return ssn;
+};
+const parseEntry = (entries) => {
+    if (Array.isArray(entries) && entries.every(isEntry)) {
+        return entries;
+    }
+    throw new Error('Incorrect or missing entry');
+};
+const parseDescription = (description) => {
+    if (!description || !isString(description)) {
+        throw new Error('Incorrect or missing ssn');
+    }
+    return description;
+};
+const parseSpecialist = (specialist) => {
+    if (!specialist || !isString(specialist)) {
+        throw new Error('Incorrect or missing specialist');
+    }
+    return specialist;
+};
+const parseDiagnosisCodes = (diagnosisCodes) => {
+    if (!diagnosisCodes || !isStringArray(diagnosisCodes)) {
+        throw new Error('incorrect or missing diagnosis array');
+    }
+    return diagnosisCodes;
+};
+const parseDischarge = (discharge) => {
+    if (!discharge || !isDischarge(discharge)) {
+        throw new Error('incorrect or missing discharge');
+    }
+    return discharge;
+};
+// const parseHealthCheckRating = (healthCheckRating: unknown): HealthCheckRating => {
+//   if (!healthCheckRating || !isHealthCheckRating(healthCheckRating) || isString(healthCheckRating)) {
+//     throw new Error('incorrect or missing healthcheckRating');
+//   }
+//   return healthCheckRating;
+// };
+exports.default = { toNewPatient, toNewEntry };
--- 9/patientor_backend/src/routes/patients.ts
+++ 9/patientor_backend/src/routes/patients.ts
@@ -3,16 +3,16 @@
 import express from 'express';
 
 import patientService from '../services/patientService';
-import toNewPatientEntry from '../utils';
+import utils from '../utils';
 
 const router = express.Router();
 
 router.get('/', (_request, response) => {
-  response.send(patientService.getEntries());
+  response.send(patientService.getPatients());
 });
 
 router.get('/:id', (request, response) => {
-  const patient = patientService.findById(Number(request.params.id));
+  const patient = patientService.findById(String(request.params.id));
 
   if (patient) {
     response.send(patient);
@@ -21,9 +21,32 @@ router.get('/:id', (request, response) => {
   }
 });
 
+router.put('/:id', (request, response) => {
+  const id = request.params.id;
+  const updatedPatient = utils.toNewPatient(request.body);
+  const entries = request.body.entries;
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+  entries.map((e: { type: unknown; }) => {
+    Object.values(e).forEach(value => {
+      if (value === '') {
+        response.status(422).json({ error: 'Incorrect data entry, check that all fields are filled' });
+        return;
+      }
+    });
+  });
+
+  const updatedPatientData = patientService.updatePatient(id, updatedPatient);
+
+  if (updatedPatientData) {
+    response.json(updatedPatientData);
+  } else {
+    response.status(404).json({ error: 'Patient not found' });
+  }
+});
+
 router.post('/', (request, response) => {
   try {
-    const newPatientEntry = toNewPatientEntry(request.body);
+    const newPatientEntry = utils.toNewPatient(request.body);
     const addedEntry = patientService.addPatient(newPatientEntry);
     response.json(addedEntry);
   } catch (error: unknown) {
